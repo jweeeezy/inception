5210027e89da9689900423e3cde03f5db3b191c5
Jakob Willert <willertjakob@gmail.com>
2024-04-21 17:20:12 +0000

    [mariadb] adds setup script for mariadb/wordpress

--- srcs/requirements/mariadb/tools/setup_mariadb_for_wordpress.sh
+++ srcs/requirements/mariadb/tools/setup_mariadb_for_wordpress.sh
@@ -1,6 +1,10 @@
 
 # mb chmod // chown -R following folders: /run/mysqld /var/lib/mysql /var/log/mysql
 
+mariadb -e"CREATE DATABASE IF NOT EXISTS $MARIADB_DB_NAME"
+mariadb -e"CREATE USER IF NOT EXISTS '$MARIADB_USER_NAME'@'%' IDENTIFIED BY '$MARIADB_USER_PASSWORD'"
+mariadb -e"GRANT ALL PRIVILEGES ON $MARIADB_DB_NAME TO '$MARIADB_USER_NAME'@'%' WITH GRANT OPTION; FLUSH PRIVILEGES;"
+
 # create a database for wordpress (with env variable)
 # create a wordpress database admin + passwd
 # create a wordpress user + passwd (readonly)
@@ -17,4 +21,3 @@
 # CREATE USER 'pleb'@'%' IDENTIFIED BY 'some_pass';
 # GRANT SELECT, INSERT, UPDATE ON 'database-name'.* TO 'pleb'@'%';
 # FLUSH PRIVILEGES;
-
