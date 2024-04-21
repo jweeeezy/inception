ee1d3321dc4532c306655048f78490fe3618ed5b
Jakob Willert <willertjakob@gmail.com>
2024-04-21 12:37:48 +0000

    [wordpress] adds workdir and downloads wordpress

--- srcs/requirements/wordpress/tools/wp_install.sh
+++ srcs/requirements/wordpress/tools/wp_install.sh
@@ -5,3 +5,4 @@
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 chmod +x wp-cli.phar
 mv wp-cli.phar /usr/local/bin/wp
+wp core download --path=/var/www/html --allow-root
