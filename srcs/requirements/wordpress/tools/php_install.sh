c1525a1da875bf405417044399ae900fa85bc132
Jakob Willert <willertjakob@gmail.com>
2024-04-25 15:02:50 +0000

    [wordpress] implements ENTRYPOINT with php-fpm, simplifies Dockerfile

--- srcs/requirements/wordpress/tools/php_install.sh
+++ srcs/requirements/wordpress/tools/php_install.sh
@@ -6,6 +6,5 @@
 apt-get install apt-transport-https lsb-release curl -y
 curl -sSLo /usr/share/keyrings/deb.sury.org-php.gpg https://packages.sury.org/php/apt.gpg
 sh -c 'echo "deb [signed-by=/usr/share/keyrings/deb.sury.org-php.gpg] https://packages.sury.org/php/ $(lsb_release -sc) main" > /etc/apt/sources.list.d/php.list'
-apt-get update -y && apt upgrade -y
+apt-get update -y && apt-get upgrade -y
 apt-get install -y php8.3 php8.3-fpm php8.3-mysql php8.3-cli php8.3-{bz2,curl,mbstring,intl}
-echo "Script ran successfully!" 1>&2 
