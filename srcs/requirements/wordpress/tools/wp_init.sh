1176699db09bb9d645f161954c43a32cd1c99d20
Jakob Willert <willertjakob@gmail.com>
2024-05-01 17:52:13 +0000

    [wordpress] switch to localhost, adds mariadb_root_password

--- srcs/requirements/wordpress/tools/wp_init.sh
+++ srcs/requirements/wordpress/tools/wp_init.sh
@@ -2,14 +2,20 @@
 
 WORDPRESS_ADMIN_PASSWORD=$(cat /run/secrets/wordpress_admin_password)
 WORDPRESS_USER_PASSWORD=$(cat /run/secrets/wordpress_user_password)
+MARIADB_ROOT_PASSWORD=$(cat /run/secrets/mariadb_root_password)
 
 rm /var/www/html/wp-config.php || true
 
-wp config create --allow-root --path="/var/www/html" --url=jwillert.42.fr \
-    --dbname="$MARIADB_DB_NAME" --dbuser=root
-wp core install --allow-root --path="/var/www/html" --url=jwillert.42.fr \
+# @note mb add a healthcheck instead
+#sleep 10
+
+wp config create --allow-root --path="/var/www/html" \
+    --dbname="$MARIADB_DB_NAME" --dbuser="$MARIADB_ROOT_USER_NAME" --dbpass="$MARIADB_ROOT_PASSWORD" --dbhost=mariadb
+
+# @note swap out https://localhost
+wp core install --allow-root --path="/var/www/html" --url=https://localhost \
 	--admin_user="$WORDPRESS_ADMIN_NAME" --admin_password="$WORDPRESS_ADMIN_PASSWORD" \
-	--admin_email="jwillert@student.42heilbronn.de" --title="wp-title"
+	--admin_email="jwillert@student.42heilbronn.de" --title="inception" --skip-email
 wp user create --allow-root $WORDPRESS_USER_NAME "willertjakob@gmail.com" \
     --role=author --user_pass="$WORDPRESS_USER_PASSWORD"
 
