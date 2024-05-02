40bda2a6dc417f0cd781cad04cc56453534f0a3a
Jakob Willert <willertjakob@gmail.com>
2024-05-02 09:37:10 +0000

    [nginx / wordpress] changes localhost to jwillert.42.fr, removes @notes

--- srcs/requirements/wordpress/tools/wp_init.sh
+++ srcs/requirements/wordpress/tools/wp_init.sh
@@ -9,8 +9,7 @@ rm /var/www/html/wp-config.php || true
 wp config create --allow-root --path="/var/www/html" \
     --dbname="$MARIADB_DB_NAME" --dbuser="$MARIADB_ROOT_USER_NAME" --dbpass="$MARIADB_ROOT_PASSWORD" --dbhost=mariadb
 
-# @note swap out https://localhost
-wp core install --allow-root --path="/var/www/html" --url=https://localhost \
+wp core install --allow-root --path="/var/www/html" --url=https://jwillert.42.fr \
 	--admin_user="$WORDPRESS_ADMIN_NAME" --admin_password="$WORDPRESS_ADMIN_PASSWORD" \
 	--admin_email="jwillert@student.42heilbronn.de" --title="inception" --skip-email
 wp user create --allow-root $WORDPRESS_USER_NAME "willertjakob@gmail.com" \
